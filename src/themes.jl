abfa83762684ae63b40b022bf1c1cc6797f98892
Christian Busch <Christian.Busch@ruhr-uni-bochum.de>
2023-11-20 22:54:51 +0100

    Initial commit

---
+++ src/themes.jl
@@ -0,0 +1,79 @@
+module MyMakieThemes
+
+export get_theme
+
+using CairoMakie
+using MathTeXEngine
+using Colors
+
+standard_theme_colors = [parse(Colorant, c) for c in ["#dd3218", "#19a525", "#1b55cf", "#b75fb3", "#000000"]]
+
+const unit_to_pt = Dict(
+    :mm => 2.83,
+    :cm => 28.3,
+    :pt => 1,
+    :inch => 72,
+)
+
+function get_theme(
+    unit =:mm,
+    resolution = (85, 60),
+    fontsize = 12,
+    figure_padding = 15,
+    colors = standard_theme_colors,
+    Nxminorticks = 2,
+    Nyminorticks = 2
+    )
+
+    spine_width = 0.75
+
+    theme = Theme(;
+        CairoMakie = (pt_per_unit = 1, px_per_unit = 4),
+        GLMakie    = (px_per_unit = 1,),
+        resolution = resolution .* unit_to_pt[unit],
+        fontsize,
+        figure_padding,
+        palette = (
+            color  = colors, 
+            marker = Cycle([:circle, :rect, :utriangle]),
+        ),
+        Lines = (
+            linewidth = 1.25,
+            linestyle = :solid,
+        ),
+        Scatter = (
+            markersize = 6,
+        ),
+        Legend = (
+            patchsize    = (20.0, 5.0), 
+            rowgap       = 5, 
+            framewidth   = spine_width, 
+            labelfont    = texfont(), 
+            framevisible = false,
+            padding      = (5.0, 5.0, 3.0, 3.0)
+        ),
+        Axis = (
+            xlabelfont         = texfont(),
+            ylabelfont         = texfont(),
+            xticklabelfont     = texfont(),
+            yticklabelfont     = texfont(),
+            xgridvisible       = false,
+            ygridvisible       = false,
+            xminorticksvisible = true,
+            yminorticksvisible = true,
+            xticksize          = 4,
+            yticksize          = 4,
+            xminorticksize     = 2,
+            yminorticksize     = 2,
+            xminorticks        = IntervalsBetween(Nxminorticks),
+            yminorticks        = IntervalsBetween(Nyminorticks),
+            spinewidth         = spine_width,
+            xtickwidth         = spine_width,
+            ytickwidth         = spine_width,
+            xminortickwidth    = spine_width,
+            yminortickwidth    = spine_width,
+        )
+    )
+    return theme
+end
+end
